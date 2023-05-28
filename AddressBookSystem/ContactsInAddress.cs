af3e0d2412ee75849f4a15b8e42f6552b529b0ab
Virtapatil2000 <virtapatil01@gmail.com>
2023-05-28 23:34:28 +0530

    Adding UC1-Contacts In Address

---
+++ AddressBookSystem/ContactsInAddress.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace AddressBookSystem
+{
+    public class ContactsInAddress
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+
+    class AddressBook
+    {
+        private List<ContactsInAddress> contacts;
+
+        public AddressBook()
+        {
+            contacts = new List<ContactsInAddress>();
+        }
+
+        public void AddContact(ContactsInAddress contact)
+        {
+            contacts.Add(contact);
+        }
+
+        public void DisplayContacts()
+        {
+            foreach (var contact in contacts)
+            {
+                Console.WriteLine($"First Name: {contact.FirstName}");
+                Console.WriteLine($"Last Name: {contact.LastName}");
+                Console.WriteLine($"Address: {contact.Address}");
+                Console.WriteLine($"City: {contact.City}");
+                Console.WriteLine($"State: {contact.State}");
+                Console.WriteLine($"ZIP: {contact.Zip}");
+                Console.WriteLine($"Phone Number: {contact.PhoneNumber}");
+                Console.WriteLine($"Email: {contact.Email}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            AddressBook addressBook = new AddressBook();
+
+            // Create sample contacts
+            ContactsInAddress contact1 = new ContactsInAddress()
+            {
+                FirstName = "vir",
+                LastName = "patil",
+                Address = "Jannat",
+                City = "Ngp",
+                State = "NY",
+                Zip = "10001",
+                PhoneNumber = "1234567890",
+                Email = "kyahilikhhu@gmail.com"
+            };
+
+            ContactsInAddress contact2 = new ContactsInAddress()
+            {
+                FirstName = "Virta",
+                LastName = "Patil",
+                Address = "Jha koi aata jata nahi",
+                City = "Jadu ki Nagari",
+                State = "Maharashtra",
+                Zip = "90001",
+                PhoneNumber = "7709523217",
+                Email = "vir33@gmail.com"
+            };
+
+            // Add contacts to the address book
+            addressBook.AddContact(contact1);
+            addressBook.AddContact(contact2);
+
+            // Display all contacts in the address book
+            addressBook.DisplayContacts();
+
+            Console.ReadLine();
         }
     }
 }
